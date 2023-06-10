145d8a543d0cb616b968fce085746d0629a1103e
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-06-10 11:21:59 +0200

    Add files via upload

--- php/contact_form_mail.php
+++ php/contact_form_mail.php
@@ -3,8 +3,7 @@
   $nachname = $_GET['mail'];
   $content = $_GET['content'];
 
+  $msg = "$name $nachname\n $content"
 
-
-
-
+  mail("lipka.bastian04@gmail.com", "@shademedia mail form $name $nachname", $msg);
 ?>
