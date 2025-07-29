57155e742f11b28c79150324d4b9e96c36a7238d
Bastian Lipka <lipka.bastian04@gmail.com>
2025-07-28 23:54:01 -0700

    fix: fixed the footer and the navbar

--- php/contact_form_mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $name = $_GET['name'];
-  $nachname = $_GET['mail'];
-  $content = $_GET['content'];
-
-  $msg = "$name $nachname\n $content"
-
-  mail("lipka.bastian04@gmail.com", "@shademedia mail form $name $nachname", $msg);
-?>
